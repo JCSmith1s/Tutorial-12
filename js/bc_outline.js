f1758fd3a875b1bcbb443da6eec4aa5f70b345a1
JCSmith1s <JCSmith1s@capetigers.com>
2026-02-23 10:40:15 -0600

    make it to section 12.2

--- js/bc_outline.js
+++ js/bc_outline.js
@@ -5,8 +5,8 @@
    Tutorial 12
    Tutorial Case
 
-   Author: 
-   Date:   
+   Author: Josiah Smith
+   Date: 2/23/26
 
    Filename: bc_outline.js
 
@@ -27,3 +27,69 @@
 
 */
 
+/*Generate an outline based on h1 through h6 headings in the source document*/
+
+window.addEventListener("load", makeOutline);
+
+function makeOutline() {
+  // Location of the document outline
+  var outline = document.getElementById("outline");
+
+  // source document for the outline
+  var source = document.getElementById("doc");
+
+  var mainHeading = document.createElement("h1");
+  var outlineList = document.createElement("ol");
+  var headingText = document.createTextNode("Outline");
+
+  mainHeading.appendChild(headingText);
+  outline.appendChild(mainHeading);
+  outline.appendChild(outlineList);
+  }
+  
+  function createList(source, outlineList) {
+    /* Loop through all of the child nodes of source article until no child nodes are left */
+
+    for (var n = source.firstChild; n !== null; n = n.nextSibling) {
+      
+    }
+
+  createList(source, outlineList);
+}
+
+function createList(source, outlineList) {
+  // Headings for the outline
+  var headings = ["H1", "H2", "H3", "H4", "H5", "H6"];
+
+  // Previous Level of the Headings
+  var prevLevel = 0;
+
+  /* Loop through all of the child nodes of source article until no child nodes are left */
+
+  for (var n = source.firstChild; n !== null; n = n.nextSibling) {
+    // Examine only article headings
+    var headLevel = headings.indexOf(n.nodeName);
+
+    if (headLevel !== -1) {
+      var listElem = document.createElement("li");
+      listElem.innerHTML = n.firstChild.nodeValue;
+      outlineList.appendChild(listElem);
+
+    }
+
+    if (headLevel === prevLevel) {
+      // Append the list item to the current list
+      outlineList.appendChild(listElem);
+    } else if (headLevel > prevLevel) {
+      // Start a new nested list
+      var nestedList = document.createElement("ol");
+      nestedList.appendChild(listElem);
+      // Append nested list to last item in the current list
+      outlineList.lastChild.appendChild(nestedList);
+      // Change the current list to the nested list
+      outlineList = nestedList;
+    } else {
+      // Append the list item to a higher list
+    }
+  }
+}
